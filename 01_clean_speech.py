f0cef8683adc9e9843cb39fc360c420278f481db
Peter Bachman <peter.j.bachman@gmail.com>
2022-12-06 13:51:47 -0600

    Figuring out how to access embeddings in SBERT

--- 01_clean_speech.py
+++ 01_clean_speech.py
@@ -6,10 +6,8 @@ Python - 3.11.0
 Pandas - 1.5.2
 """
 import pandas as pd
-from nltk import tokenize
-from transformers import BertTokenizerFast
-
-tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased')
+import numpy as np
+from sentence_transformers import SentenceTransformer, util
 
 # Load in text
 # Encoding issues are replaced with the hexidecimal form of the word
@@ -18,12 +16,31 @@ df = pd.read_csv("/Users/peter/Data/floor_speech/hein-daily/speeches_113.txt",
                  encoding="us-ascii",
                  encoding_errors='backslashreplace')
 
+# Define Model - Here is SBERT
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
 # Gets the speech
-df.loc[6002, "speech"]
+speech_1 = df.loc[6002, "speech"]
+speech_2 = df.loc[6003, "speech"]
 
-# Tokenizes one line of the dataset
-tokenizer(df.loc[6002, "speech"])
+# Remove punctuation, since OCR is not good at identifying periods and commas
+# accurately
+speech_cleaned_1 = speech_1.replace(".", "")
+speech_cleaned_1 = speech_cleaned_1.replace(",", "")
 
-# Okay the OCR maybe has issues with punctuation. Should I just remove
-# punctuation then?
-tokenize.sent_tokenize(df.loc[6002, "speech"])
+speech_cleaned_2 = speech_2.replace(".", "")
+speech_cleaned_2 = speech_cleaned_2.replace(",", "")
+
+# Convert sentences to sentence-level embeddings
+test_1 = model.encode(speech_cleaned_1)
+test_2 = model.encode(speech_cleaned_2)
+
+# Cosine Similarity. Here we go.
+util.pytorch_cos_sim(test_1, test_2).numpy()[0][0]
+
+# I need to fine tune the model I think. But on how much of the data?
+# What is the lower cutoff for sentence length where I decide it's a formality?
+
+# Embedding objects are numpy arrays where the dtype is np.float32
+
+np.array([(1, 2), (3, 4)], dtype=np.float32)
