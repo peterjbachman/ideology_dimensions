803ab9241fece5785abe0484c0edb80bb5f73594
Peter Bachman <peter.j.bachman@gmail.com>
2022-12-13 11:13:38 -0600

    Pretty up the plots a bit

--- 03_plot_data.R
+++ 03_plot_data.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(ggthemes)
 library(data.table)
 
 # Read in data
@@ -17,11 +18,17 @@ test <- df[party == "D", .(count = .N, mean_sim = mean_sim, session = session),
   by = .(name)
 ]
 df_test <- test[count > 19]
+df_test <- df_test[name %in% unique(df_test$name)[1:4]]
 
-# Plots of some specific legislators that show up more than 19 times.
+# Plots of some legislators that show up more than 19 times.
 ggplot(data = df_test, aes(x = session, y = mean_sim, color = name)) +
   geom_line() +
-  theme_linedraw()
+  labs(
+    x = "Session of Congress",
+    y = "Similarity to Universalism or Populism",
+    color = "Name of Legislator"
+  ) +
+  theme_clean()
 
 # Plots grouped by Party
 df_grouped_party <- df[party %in% c("D", "R"),
@@ -31,7 +38,13 @@ df_grouped_party <- df[party %in% c("D", "R"),
 
 ggplot(data = df_grouped_party, aes(x = session, y = mean_sim, color = party)) +
   geom_line() +
-  theme_linedraw()
+  labs(
+    x = "Session of Congress",
+    y = "Similarity to Universalism or Populism",
+    color = "Political Party"
+  ) +
+  theme_clean() +
+  scale_color_gdocs(labels = c("Democrat", "Republican"))
 
 # Plots grouped by chamber in Democratic Party
 df_grouped_chamber <- df[party == "D",
@@ -44,4 +57,10 @@ ggplot(
   aes(x = session, y = mean_sim, color = chamber)
 ) +
   geom_line() +
-  theme_linedraw()
+  labs(
+    x = "Session of Congress",
+    y = "Similarity to Universalism or Populism",
+    color = "Legislative Chamber"
+  ) +
+  theme_clean() +
+  scale_color_few(labels = c("House", "Senate"))
